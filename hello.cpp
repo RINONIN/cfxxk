25b165606693842924fb5de081046a01831c8022
RINONIN <779057748@qq.com>
2021-10-03 19:45:12 +0800

    init

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+using namespace std;
 
 int main()
  {
-
+  //屏幕输出hello world（单行注释）
 	std::cout << "Hello World!" << std::endl;
-
+ 
 	return 0;
 }
+
+/*多行注释
+abababababab
+ababbabababa
+*/
